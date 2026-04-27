04a474c0e8c5abe3dbbddeb0098606ae822c7d70
DangHoangLong <long.711.sg@gmail.com>
2026-04-27 10:14:49 +0700

    update

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@ basic.forever(function () {
     lcd.displayText(convertToText(pins.analogReadPin(AnalogPin.P0)), 1, 1)
     basic.pause(100)
     if (pins.analogReadPin(AnalogPin.P0) <= 100) {
-        lcd.displayText("Nguy hiem!!!", 1, 1)
+        lcd.displayText("Nguy hiem!!!", 1, 2)
         basic.showIcon(IconNames.Angry)
     }
     if (pins.analogReadPin(AnalogPin.P0) > 100) {
-        lcd.displayText("Can Than!!!", 1, 1)
+        lcd.displayText("Can Than!!!", 1, 2)
         basic.showIcon(IconNames.Surprised)
     }
     if (pins.analogReadPin(AnalogPin.P0) > 200) {
-        lcd.displayText("An Toan", 1, 1)
+        lcd.displayText("An Toan", 1, 2)
         basic.showIcon(IconNames.Happy)
     }
 })
