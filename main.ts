92727e7e3aa99db6e5e653e1c2925985cee62653
DangHoangLong <long.711.sg@gmail.com>
2026-04-27 10:11:00 +0700

    Interact with LCD & 5x5 LED

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
 basic.forever(function () {
     lcd.displayText(convertToText(pins.analogReadPin(AnalogPin.P0)), 1, 1)
-    basic.pause(1000)
+    basic.pause(100)
+    if (pins.analogReadPin(AnalogPin.P0) <= 100) {
+        lcd.displayText("Nguy hiem!!!", 1, 1)
+        basic.showIcon(IconNames.Angry)
+    }
+    if (pins.analogReadPin(AnalogPin.P0) > 100) {
+        lcd.displayText("Can Than!!!", 1, 1)
+        basic.showIcon(IconNames.Surprised)
+    }
+    if (pins.analogReadPin(AnalogPin.P0) > 200) {
+        lcd.displayText("An Toan", 1, 1)
+        basic.showIcon(IconNames.Happy)
+    }
 })
