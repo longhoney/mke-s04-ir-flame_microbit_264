6ca5f990fe111d30cf53aa1d7f7435e5ff24be3b
DangHoangLong <long.711.sg@gmail.com>
2026-05-03 08:35:52 +0700

    refresh LCD

--- main.ts
+++ main.ts
@@ -1,16 +1,6 @@
 basic.forever(function () {
-    lcd.displayText(convertToText(pins.analogReadPin(AnalogPin.P0)), 1, 1)
-    basic.pause(100)
-    if (pins.analogReadPin(AnalogPin.P0) <= 100) {
-        lcd.displayText("Nguy hiem!!!", 1, 2)
-        basic.showIcon(IconNames.Angry)
-    }
-    if (pins.analogReadPin(AnalogPin.P0) > 100) {
-        lcd.displayText("Can Than!!!", 1, 2)
-        basic.showIcon(IconNames.Surprised)
-    }
-    if (pins.analogReadPin(AnalogPin.P0) > 200) {
-        lcd.displayText("An Toan", 1, 2)
-        basic.showIcon(IconNames.Happy)
-    }
+    lcd.clearScreen()
+    lcd.displayText("MKE-S04 Value:", 1, 1)
+    lcd.displayText(convertToText(pins.analogReadPin(AnalogPin.P0)), 1, 2)
+    basic.pause(1000)
 })
